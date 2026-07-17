97c6c4d0164f3679bb1c3efe282ceff609dd312d
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 00:01:10 +0330

    Create LiveStatusChip.kt

---
+++ app/src/main/java/com/mydgnbot/ui/components/LiveStatusChip.kt
@@ -0,0 +1,22 @@
+package com.mydgnbot.ui.components
+
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LiveStatusChip() {
+
+    AssistChip(
+
+        onClick = {},
+
+        label = {
+
+            Text("🟢 Monitoring")
+
+        }
+
+    )
+
+}
