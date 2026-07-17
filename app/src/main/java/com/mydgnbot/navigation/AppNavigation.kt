eb6f2eee4a4708a46894601e575901d17090fae7
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 00:54:56 +0330

    Update AppNavigation.kt

--- app/src/main/java/com/mydgnbot/navigation/AppNavigation.kt
+++ app/src/main/java/com/mydgnbot/navigation/AppNavigation.kt
@@ -1,34 +1,65 @@
 package com.mydgnbot.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.mydgnbot.ui.screens.HomeScreen
 import com.mydgnbot.ui.screens.HistoryScreen
 import com.mydgnbot.ui.screens.SettingsScreen
 
-@Composable
-fun AppNavigation() {
 
-    val navController = rememberNavController()
+@Composable
+fun AppNavigation(
+
+    navController: NavHostController
+
+) {
+
 
     NavHost(
+
         navController = navController,
+
         startDestination = Screen.Home.route
+
     ) {
 
-        composable(Screen.Home.route) {
+
+        composable(
+
+            Screen.Home.route
+
+        ) {
+
             HomeScreen()
+
         }
 
-        composable(Screen.History.route) {
+
+
+        composable(
+
+            Screen.History.route
+
+        ) {
+
             HistoryScreen()
+
         }
 
-        composable(Screen.Settings.route) {
+
+
+        composable(
+
+            Screen.Settings.route
+
+        ) {
+
             SettingsScreen()
+
         }
+
 
     }
 
