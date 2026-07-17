7c1e58966cd3bded17df17bce7b8ab4d28aa092a
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 00:11:31 +0330

    Create Animations.kt

---
+++ app/src/main/java/com/mydgnbot/ui/design/Animations.kt
@@ -0,0 +1,15 @@
+package com.mydgnbot.ui.design
+
+object MyDGNAnimations {
+
+    // Fast because this app is speed focused
+
+    const val Instant = 100
+
+    const val Fast = 200
+
+    const val Normal = 300
+
+    const val Slow = 500
+
+}
