f51c714ec4428b9eccc6a26679d2effc6a660787
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 00:10:24 +0330

    Create Dimens.kt

---
+++ app/src/main/java/com/mydgnbot/ui/design/Dimens.kt
@@ -0,0 +1,33 @@
+package com.mydgnbot.ui.design
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+
+    // Screen
+    val ScreenPadding = 20.dp
+    val SmallPadding = 8.dp
+    val MediumPadding = 12.dp
+    val LargePadding = 24.dp
+
+    // Cards
+    val CardCornerRadius = 20.dp
+    val SmallCardRadius = 16.dp
+
+    // Buttons
+    val ButtonHeight = 48.dp
+    val ButtonCornerRadius = 24.dp
+
+    // Chips
+    val ChipHeight = 36.dp
+
+    // Player Card
+    val PlayerImageWidth = 90.dp
+    val PlayerImageHeight = 110.dp
+
+    // Icons
+    val SmallIcon = 18.dp
+    val MediumIcon = 24.dp
+    val LargeIcon = 48.dp
+
+}
