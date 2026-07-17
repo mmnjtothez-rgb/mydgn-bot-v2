d7879da8798aefb16cab26a0a8e06e920fdd1143
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 01:12:36 +0330

    Create Player.kt

---
+++ app/src/main/java/com/mydgnbot/data/model/Player.kt
@@ -0,0 +1,30 @@
+package com.mydgnbot.data.model
+
+
+data class Player(
+
+    val name: String,
+
+    val rating: Int,
+
+    val position: String,
+
+    val startingBid: String,
+
+    val buyNow: String,
+
+    val chemistryStyle: String,
+
+    val owners: Int,
+
+    val marketTime: String,
+
+    val account: String,
+
+    val payment: String,
+
+    val transactionId: String,
+
+    val myDgnPrice: String
+
+)
