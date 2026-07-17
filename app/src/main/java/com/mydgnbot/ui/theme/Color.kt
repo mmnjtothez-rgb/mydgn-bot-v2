f319e0186807128e4d25a5efca32c9c94b8e6254
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-17 23:36:50 +0330

    Create Color.kt

---
+++ app/src/main/java/com/mydgnbot/ui/theme/Color.kt
@@ -0,0 +1,35 @@
+package com.mydgnbot.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme
+val Primary = Color(0xFF3563F6)
+val PrimaryContainer = Color(0xFFE8F0FF)
+
+val Secondary = Color(0xFF6C63FF)
+
+val Background = Color(0xFFF8FAFC)
+val Surface = Color(0xFFFFFFFF)
+
+val SurfaceVariant = Color(0xFFF1F5F9)
+
+val OnPrimary = Color.White
+val OnBackground = Color(0xFF111827)
+val OnSurface = Color(0xFF111827)
+
+// Dark Theme
+val DarkPrimary = Color(0xFF4F7CFF)
+val DarkPrimaryContainer = Color(0xFF223A72)
+
+val DarkBackground = Color(0xFF0B1120)
+val DarkSurface = Color(0xFF111827)
+
+val DarkSurfaceVariant = Color(0xFF1F2937)
+
+val OnDarkBackground = Color(0xFFF8FAFC)
+val OnDarkSurface = Color(0xFFF8FAFC)
+
+// Status Colors
+val Success = Color(0xFF22C55E)
+val Error = Color(0xFFEF4444)
+val Warning = Color(0xFFF59E0B)
