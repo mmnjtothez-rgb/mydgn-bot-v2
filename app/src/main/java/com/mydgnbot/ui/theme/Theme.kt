cfbbaece177f174bee5b7f8f846437c56e2c64f8
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 00:12:07 +0330

    Update Theme.kt

--- app/src/main/java/com/mydgnbot/ui/theme/Theme.kt
+++ app/src/main/java/com/mydgnbot/ui/theme/Theme.kt
@@ -5,42 +5,68 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import com.mydgnbot.ui.design.MyDGNShapes
 
 private val LightColors = lightColorScheme(
+
     primary = Primary,
+
     primaryContainer = PrimaryContainer,
 
     secondary = Secondary,
 
     background = Background,
+
     surface = Surface,
 
     onPrimary = OnPrimary,
+
     onBackground = OnBackground,
+
     onSurface = OnSurface
+
 )
 
+
 private val DarkColors = darkColorScheme(
+
     primary = DarkPrimary,
+
     primaryContainer = DarkPrimaryContainer,
 
     background = DarkBackground,
+
     surface = DarkSurface,
 
     onBackground = OnDarkBackground,
+
     onSurface = OnDarkSurface
+
 )
+
 
 @Composable
 fun MyDGNTheme(
+
     darkTheme: Boolean = isSystemInDarkTheme(),
+
     content: @Composable () -> Unit
+
 ) {
 
     MaterialTheme(
-        colorScheme = if (darkTheme) DarkColors else LightColors,
+
+        colorScheme = if (darkTheme)
+            DarkColors
+        else
+            LightColors,
+
         typography = Typography,
+
+        shapes = MyDGNShapes,
+
         content = content
+
     )
 
 }
