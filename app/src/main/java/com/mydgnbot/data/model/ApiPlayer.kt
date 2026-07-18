7bbc12afb74eed2fc5b5cf344a29b99bb0d236f3
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 14:16:20 +0330

    Create ApiPlayer.kt

---
+++ app/src/main/java/com/mydgnbot/data/model/ApiPlayer.kt
@@ -0,0 +1,40 @@
+package com.mydgnbot.data.model
+
+
+data class ApiPlayer(
+
+    val code: Int,
+
+    val transactionID: Int,
+
+    val tradeID: String,
+
+    val status: String,
+
+    val platform: String,
+
+    val assetID: Long,
+
+    val resourceID: Long,
+
+    val baseID: Long,
+
+    val playerName: String,
+
+    val rating: Int,
+
+    val startPrice: Int,
+
+    val coinAmount: Int,
+
+    val paymentInUsd: Double,
+
+    val cardValue: Int,
+
+    val ea_expires_at: Long,
+
+    val owners: Int,
+
+    val chemistry_style: String?
+
+)
