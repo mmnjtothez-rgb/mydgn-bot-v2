6b433e9db6f9ea98cba04236d25d80a6da609e3e
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 00:10:13 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,6 @@ plugins {
         apply false
 
 
-    id("com.android.library")
-        version "8.7.3"
-        apply false
-
-
     id("org.jetbrains.kotlin.android")
         version "2.0.21"
         apply false
