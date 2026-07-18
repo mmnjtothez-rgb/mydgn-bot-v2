5827006e6bd10f646b5b4922d4807a0dafd877ab
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 20:51:48 +0330

    Create Shape.kt

---
+++ app/src/main/java/com/mydgnbot/ui/theme/Shape.kt
@@ -0,0 +1,18 @@
+package com.mydgnbot.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object AppShapes {
+
+    val Small = RoundedCornerShape(8.dp)
+
+    val Medium = RoundedCornerShape(12.dp)
+
+    val Large = RoundedCornerShape(16.dp)
+
+    val ExtraLarge = RoundedCornerShape(20.dp)
+
+    val Pill = RoundedCornerShape(50)
+
+}
