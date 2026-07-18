a181e897fce0f2d5048c31db81c0da2f67a0436f
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 20:50:51 +0330

    Create Spacing.kt

---
+++ app/src/main/java/com/mydgnbot/ui/theme/Spacing.kt
@@ -0,0 +1,19 @@
+package com.mydgnbot.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppSpacing {
+
+    val ExtraSmall = 4.dp
+
+    val Small = 8.dp
+
+    val Medium = 12.dp
+
+    val Large = 16.dp
+
+    val ExtraLarge = 24.dp
+
+    val Section = 32.dp
+
+}
