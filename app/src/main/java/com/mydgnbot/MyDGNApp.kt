2c847545af5bc8dc56a23c1fc4a8b9c0ea11c952
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 23:44:01 +0330

    Update MyDGNApp.kt

--- app/src/main/java/com/mydgnbot/MyDGNApp.kt
+++ app/src/main/java/com/mydgnbot/MyDGNApp.kt
@@ -1,37 +1,8 @@
 package com.mydgnbot
 
-import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
-import com.mydgnbot.navigation.AppNavigation
-import com.mydgnbot.ui.main.MainScaffold
-import com.mydgnbot.ui.theme.MyDGNTheme
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 
-@Composable
-fun MyDGNApp() {
-
-    MyDGNTheme {
-
-
-        val navController =
-            rememberNavController()
-
-
-        MainScaffold(
-
-            navController = navController
-
-        ) {
-
-
-            AppNavigation(
-
-                navController = navController
-
-            )
-
-        }
-
-    }
-
-}
+@HiltAndroidApp
+class MyDGNApp : Application()
