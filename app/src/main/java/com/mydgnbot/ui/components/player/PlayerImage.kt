87690e0f7196f1b3b5b6c528b6684b96a2018a77
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 01:33:15 +0330

    Update PlayerImage.kt

--- app/src/main/java/com/mydgnbot/ui/components/player/PlayerImage.kt
+++ app/src/main/java/com/mydgnbot/ui/components/player/PlayerImage.kt
@@ -2,11 +2,12 @@ package com.mydgnbot.ui.components.player
 
 
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 
 
@@ -14,33 +15,28 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun PlayerImage(
 
-    imageUrl: String?
+    imageUrl: String?,
+
+    modifier: Modifier = Modifier
 
 ) {
 
 
     Card(
 
-        modifier = Modifier
+        modifier = modifier.size(
 
-            .size(
-                width = 90.dp,
-                height = 110.dp
-            )
+            width = 90.dp,
 
-            .clip(
-                RoundedCornerShape(12.dp)
-            )
+            height = 110.dp
+
+        )
 
     ) {
 
 
         Text(
-
-            text = "Player",
-
-            modifier = Modifier
-
+            text = "Player"
         )
 
 
