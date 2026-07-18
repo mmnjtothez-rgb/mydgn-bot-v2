f7a009a8fd3563a92893895b3c577c646c2fdc71
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 13:43:26 +0330

    Create PreviewPlayer.kt

---
+++ app/src/main/java/com/mydgnbot/data/model/PreviewPlayer.kt
@@ -0,0 +1,33 @@
+package com.mydgnbot.data.model
+
+val PreviewPlayer = Player(
+
+    id = "1",
+
+    name = "Serhou Guirassy",
+
+    rating = 87,
+
+    position = "ST",
+
+    chemistryStyle = "None",
+
+    owners = 2,
+
+    marketTime = "59:42",
+
+    startingBid = 2400,
+
+    buyNow = 3900,
+
+    account = "blueonewalter@gmail.com",
+
+    payment = "PayPal",
+
+    transactionId = "#845291",
+
+    myDgnPrice = "850K",
+
+    imageUrl = null
+
+)
