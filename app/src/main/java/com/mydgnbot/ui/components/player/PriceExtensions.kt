af8d8762b3f2c81c3d6876adc8975c92c4035066
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 01:35:19 +0330

    Create PriceExtensions.kt

---
+++ app/src/main/java/com/mydgnbot/ui/components/player/PriceExtensions.kt
@@ -0,0 +1,15 @@
+package com.mydgnbot.ui.components.player
+
+
+fun Int.formatCoins(): String {
+
+    return "%,d".format(this)
+
+}
+
+
+fun Long.formatCoins(): String {
+
+    return "%,d".format(this)
+
+}
