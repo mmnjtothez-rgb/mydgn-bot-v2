cfd46701884d878804b9c563d44820658297f7a0
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 01:10:14 +0330

    Update MyDGNDrawer.kt

--- app/src/main/java/com/mydgnbot/ui/components/MyDGNDrawer.kt
+++ app/src/main/java/com/mydgnbot/ui/components/MyDGNDrawer.kt
@@ -1,6 +1,8 @@
 package com.mydgnbot.ui.components
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.unit.dp
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
