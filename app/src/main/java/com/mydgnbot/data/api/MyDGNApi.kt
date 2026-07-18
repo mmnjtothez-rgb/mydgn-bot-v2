8f3e73b9d5cfd1aebea6856ae447b6668ad29f71
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 14:23:00 +0330

    Update MyDGNApi.kt

--- app/src/main/java/com/mydgnbot/data/api/MyDGNApi.kt
+++ app/src/main/java/com/mydgnbot/data/api/MyDGNApi.kt
@@ -1,14 +1,70 @@
+package com.mydgnbot.data.api
+
+import com.mydgnbot.data.model.ApiPlayer
+import com.mydgnbot.data.model.StatusResponse
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+
 interface MyDGNApi {
+
 
     @GET("transfers")
     suspend fun getTransfer(
-        ...
+
+        @Query("user")
+        user: String,
+
+        @Query("platform")
+        platform: String,
+
+        @Query("timestamp")
+        timestamp: Long,
+
+        @Query("hash")
+        hash: String,
+
+        @Query("maximumBuyOutPrice")
+        maximumBuyOutPrice: Int,
+
+        @Query("minimumBuyOutPrice")
+        minimumBuyOutPrice: Int,
+
+        @Query("botapp")
+        botApp: String,
+
+        @Query("playerType")
+        playerType: Int
+
     ): ApiPlayer
+
 
 
     @POST("status")
     suspend fun updateStatus(
-        ...
+
+        @Query("user")
+        user: String,
+
+        @Query("platform")
+        platform: String,
+
+        @Query("timestamp")
+        timestamp: Long,
+
+        @Query("hash")
+        hash: String,
+
+        @Query("transactionID")
+        transactionId: Int,
+
+        @Query("status")
+        status: String,
+
+        @Query("emailHash")
+        emailHash: String = ""
+
     ): StatusResponse
 
 }
