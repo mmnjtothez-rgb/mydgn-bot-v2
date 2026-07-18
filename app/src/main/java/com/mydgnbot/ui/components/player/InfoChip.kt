2d9a09f6d09046475322b411066b77c38f6781b3
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 01:50:03 +0330

    Create InfoChip.kt

---
+++ app/src/main/java/com/mydgnbot/ui/components/player/InfoChip.kt
@@ -0,0 +1,28 @@
+package com.mydgnbot.ui.components.player
+
+
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun InfoChip(
+
+    text: String
+
+) {
+
+    AssistChip(
+
+        onClick = {},
+
+        label = {
+
+            Text(text)
+
+        }
+
+    )
+
+}
