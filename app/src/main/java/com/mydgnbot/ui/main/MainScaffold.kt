9b4892a0a80dca9d807f733120d551fb800c0336
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 01:08:19 +0330

    Update MainScaffold.kt

--- app/src/main/java/com/mydgnbot/ui/main/MainScaffold.kt
+++ app/src/main/java/com/mydgnbot/ui/main/MainScaffold.kt
@@ -3,7 +3,7 @@ package com.mydgnbot.ui.main
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 
