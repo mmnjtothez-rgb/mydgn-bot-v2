18a3e4217d271ab7ed554ca4f15ccf292b1386cf
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 01:09:04 +0330

    Update SectionCard.kt

--- app/src/main/java/com/mydgnbot/ui/components/Common/SectionCard.kt
+++ app/src/main/java/com/mydgnbot/ui/components/Common/SectionCard.kt
@@ -2,6 +2,7 @@ package com.mydgnbot.ui.components.common
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.*
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
