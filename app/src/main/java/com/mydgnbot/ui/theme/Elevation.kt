1ad5f4e96164998bf086f96013326af4a85c4966
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 20:52:20 +0330

    Create Elevation.kt

---
+++ app/src/main/java/com/mydgnbot/ui/theme/Elevation.kt
@@ -0,0 +1,15 @@
+package com.mydgnbot.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppElevation {
+
+    val None = 0.dp
+
+    val Low = 2.dp
+
+    val Medium = 6.dp
+
+    val High = 10.dp
+
+}
