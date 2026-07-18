9fcca4711bfcfa8683011c9960cf5241d70ecf8d
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 20:51:22 +0330

    Create Dimensions.kt

---
+++ app/src/main/java/com/mydgnbot/ui/theme/Dimensions.kt
@@ -0,0 +1,19 @@
+package com.mydgnbot.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object AppDimensions {
+
+    val PlayerCardWidth = 90.dp
+
+    val PlayerCardHeight = 110.dp
+
+    val FutbinBadgeWidth = 82.dp
+
+    val ButtonHeight = 52.dp
+
+    val ProgressBarHeight = 6.dp
+
+    val InfoChipHeight = 32.dp
+
+}
