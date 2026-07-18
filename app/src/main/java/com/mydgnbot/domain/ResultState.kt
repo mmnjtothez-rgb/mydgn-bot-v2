03c687704ce3b550c8229d6785a9fcaad2644e84
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-18 22:14:20 +0330

    Create ResultState.kt

---
+++ app/src/main/java/com/mydgnbot/domain/ResultState.kt
@@ -0,0 +1,19 @@
+package com.mydgnbot.domain
+
+sealed class ResultState<out T> {
+
+
+    data object Loading :
+        ResultState<Nothing>()
+
+
+    data class Success<T>(
+        val data: T
+    ) : ResultState<T>()
+
+
+    data class Error(
+        val message: String
+    ) : ResultState<Nothing>()
+
+}
