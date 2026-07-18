85d3714b9fadfc8fc41d2717e1061a7cec74a751
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 01:41:46 +0330

    Create PriceBox.kt

---
+++ app/src/main/java/com/mydgnbot/ui/components/player/PriceBox.kt
@@ -0,0 +1,36 @@
+package com.mydgnbot.ui.components.player
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+
+import androidx.compose.runtime.Composable
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+
+
+@Composable
+fun PriceBox(
+
+    title: String,
+
+    price: String,
+
+    modifier: Modifier = Modifier
+
+) {
+
+
+    Card(
+
+        modifier = modifier.width(140.dp)
+
+    ) {
+
+
