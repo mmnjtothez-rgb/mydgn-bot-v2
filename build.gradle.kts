947767f4193f4470247972b55af4a7db71d9d214
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 00:23:44 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,11 @@
 plugins {
 
-    id("com.android.application")
-        version("8.7.3")
-        apply(false)
+    id("com.android.application") version "8.7.3" apply false
 
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 
-    id("org.jetbrains.kotlin.android")
-        version("2.0.21")
-        apply(false)
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 
-
-    id("org.jetbrains.kotlin.plugin.compose")
-        version("2.0.21")
-        apply(false)
-
-
-    id("com.google.dagger.hilt.android")
-        version("2.54")
-        apply(false)
+    id("com.google.dagger.hilt.android") version "2.54" apply false
 
 }
