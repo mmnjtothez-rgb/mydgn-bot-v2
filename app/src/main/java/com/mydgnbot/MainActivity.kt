2302d63a3b844667d9ec6d44f8679a4b2f102ac5
mmnjtothez-rgb <mmnjtothez@gmail.com>
2026-07-19 12:45:32 +0330

    Update MainActivity.kt

--- app/src/main/java/com/mydgnbot/MainActivity.kt
+++ app/src/main/java/com/mydgnbot/MainActivity.kt
@@ -1,39 +1,22 @@
 package com.mydgnbot
 
-
 import android.os.Bundle
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-import androidx.compose.runtime.Composable
-
-import dagger.hilt.android.AndroidEntryPoint
-
 import com.mydgnbot.ui.navigation.AppNavigation
 import com.mydgnbot.ui.theme.MyDGNTheme
-
-
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
 
-
         setContent {
-
             MyDGNTheme {
-
                 AppNavigation()
-
             }
-
         }
-
     }
-
 }
